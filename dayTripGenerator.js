6abed523f3483c79d53d1f49a3d95485a9522dad
Daniel Christine <danieljameschristine@gmail.com>
2020-08-11 18:38:06 -0500

    completed random function

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -5,32 +5,56 @@
 //I want a location to be randomly selected for my day trip.
 //I want transportation to be randomly selected for my day trip.
 //I want a restaurant to be randomly selected for my day trip.
+//Add variable for output.
+
+//Learn how to randomize. 
+
 //Combine all variables into one output.
 //Ability to reroll output.
+
+
+
 //User decides if the output is or is not "success"
 //Display trip info to console log.
 //Each function should hold a Single Responsibility. "One action"
 
 
-var location = ["Austin", "Houston", "Dallas", "San Antonio"];
+var destination = ["Austin", "Houston", "Dallas", "San Antonio"];
 var entertainment = ["Fly kites", "Dancing", "Arcade", "Skydiving"];
 var transportation = ["PogoStick", "Teleport", "Jetpack", "Crabwalk"];
 var restaurant = ["Spock Burgers", "Krusty Krab", "Torchy's", "Taco Bell"];
+var dayTrip = [];
 
-var random = Math.random(0,3);
+
+function random(){
+    return Math.floor(Math.random() * 4);
+}
+
+
+
+let randomIndex = random();
+console.log(randomIndex);
+
+console.log(destination[randomIndex]);
+
+function findEntertainment(){
+
+}
+
 
 
 function useProgram(){
 
-    function 
-    for(let i = 0; i < location.length; i++) {
-        if(location[i] = ) {
-            ;
-        }
-        else {
-           ;
-        }
+var findDestination = ("");
+
+    
+    for(var i = 0; i < location.length; i++) {
+        dayTrip.push(random);
+        
+
+
+
     }
-
-
+    console.log ("Our suggested trip is: " + dayTrip);
 }
+
