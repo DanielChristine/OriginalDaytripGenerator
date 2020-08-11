e2cdaf57652ab4498133b685cb9ec1785e54afb7
Daniel Christine <danieljameschristine@gmail.com>
2020-08-11 17:27:59 -0500

    Add block comments, begin for loop.

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -1,16 +1,36 @@
 "use strict"
 
+//List variables
+//I want entertainment to be randomly selected for my day trip.
+//I want a location to be randomly selected for my day trip.
+//I want transportation to be randomly selected for my day trip.
+//I want a restaurant to be randomly selected for my day trip.
+//Combine all variables into one output.
+//Ability to reroll output.
+//User decides if the output is or is not "success"
+//Display trip info to console log.
+//Each function should hold a Single Responsibility. "One action"
+
 
 var location = ["Austin", "Houston", "Dallas", "San Antonio"];
 var entertainment = ["Fly kites", "Dancing", "Arcade", "Skydiving"];
 var transportation = ["PogoStick", "Teleport", "Jetpack", "Crabwalk"];
 var restaurant = ["Spock Burgers", "Krusty Krab", "Torchy's", "Taco Bell"];
 
+var random = Math.random(0,3);
 
 
 function useProgram(){
 
-function
+    function 
+    for(let i = 0; i < location.length; i++) {
+        if(location[i] = ) {
+            ;
+        }
+        else {
+           ;
+        }
+    }
 
 
 }
